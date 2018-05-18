fc539bf07ea5b8a06c04a20f7560becfd5278537
David Zambo <david@dcmf.hu>
2018-05-18 10:20:25 +0200

    Flag display problem solved, DCMF logo added, OG image added, set up, published

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,38 @@ request.open("GET", DATA_URL, true);
 request.send();
 request.onload = () => {
     const response = JSON.parse(request.response);
-    console.log(response);
     render(response);
+    window.addEventListener('resize', () => render(response));
 };
 
 const render = response => {
-    const margin = {top: 50, right: 100, bottom: 50, left: 50},
+    const margin = {top: 50, right: 100, bottom: 50, left: 100},
         width = window.innerWidth,
-        height = window.innerHeight,
+        height = window.innerHeight -140,
         {nodes, links } = response;
 
-    console.log(nodes, links);
+    d3.select('.container').html('');
+    d3.select('.container')
+        .append('div')
+        .attr('id', 'tooltip')
+        .attr('class', 'hidden')
+        .append('h3')
+        .attr('id', 'countryName');
+
+    d3.select('.container').append('h1').text('National Contiguity with a Force Directed Graph');
+
     const svg = d3.select('.container')
         .append('svg')
         .attr('width', width)
         .attr('height', height);
 
-    d3.select('.container')
-        .append('svg')
-        .style("display", "none")
-        .append('svg:image')
-        .attr('id', 'flag-icons')
-        .attr('width', 256)
-        .attr('height', 176)
-        .attr('xlink:href', './images/flags.png');
+    const flags = d3.select('.container')
+        .append('div')
+        .attr('class', 'flag-container');
 
     const simulation = d3.forceSimulation(nodes)
-        .force("charge", d3.forceManyBody().strength(-10).distanceMin(85).distanceMax(width/2))
+        .force("charge", d3.forceManyBody().strength(-10))
+        .alpha(0.5)
         .force("link", d3.forceLink(links))
         .force("center", d3.forceCenter(width/2, height/2));
 
@@ -41,29 +46,30 @@ const render = response => {
         .append('line')
         .attr('class', 'link');
 
-    const node = svg.selectAll('.node')
+    const node = flags.selectAll('.node')
         .data(nodes)
         .enter()
-        .append('g')
-        .attr('clip-path', 'url(#flag-icons');
-
-    node.append('use')
-        .attr('xlink:href', "#flag-icons")
-        .attr('class', d => 'flag flag-'+d.code);
-
-    node.selectAll('use')
-        .attr('transform', function(){
-            const pos = d3.select(this)
-                .style("background-position")
-                .replace(/[px]/g, "")
-                .replace(" ", ",");
-            return `translate (${pos})`;
-        });
-
-    node.attr('transform', d => `translate(${d.x - 5}, ${d.y - 2})`)
-        .on('mouseover', d => console.log(d.country));
+        .append('div')
+        .attr('class', d => 'node flag flag-'+d.code)
+        .on('mouseover', d => {
+            d3.select('#tooltip')
+                .style('left', d.x - 30 + "px")
+                .style('top', d.y + 20 + "px")
+                .classed('hidden', false);
+            d3.select('#countryName').text(d.country);
+        })
+        .on('mouseout', d => {
+            console.log('out');
+            d3.select('#tooltip').attr('class', 'hidden');
+        })
+        .call(d3.drag()
+            .on('start', dragstarted)
+            .on('drag', dragged)
+            .on('end', dragended));
 
     simulation.on('tick', function() {
+        node.style('transform', d => `translate(${d.x - 5}px, ${d.y + 73}px)`);
+
         link.attr('x1', d => d.source.x)
             .attr('y1', d => d.source.y)
             .attr('x2', d => d.target.x)
@@ -73,4 +79,27 @@ const render = response => {
             .attr('y', d => d.y);
 
     });
+
+    function dragstarted(d){
+        if (!d3.event.active)
+            simulation.alphaTarget(0.1).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d){
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d){
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+    d3.select('.container')
+        .append('footer')
+        .html('<a href="https://www.dcmf.hu" target="_blank"><span>codedBy</span><img src="https://www.dcmf.hu/images/dcmf-letters.png" alt="dcmf-logo" /></a>');
+
 };
